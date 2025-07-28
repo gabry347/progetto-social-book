95612425c792d4c7aa6a590170a0a2fc34507ae8
Gabriele Inglese <gabriele.inglese@excentio.com>
2025-07-28 14:15:59 +0200

    Inizializzazione backend Spring Boot

---
+++ social-book-network/src/main/java/com/bsn/book/SocialBookNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.bsn.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBookNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBookNetworkApplication.class, args);
+	}
+
+}
